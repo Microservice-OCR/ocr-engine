aaa78ff487ee191363d0d574d130113638769155
MatthieuLvsr <72619668+MatthieuLvsr@users.noreply.github.com>
2024-02-13 12:19:19 +0100

    add sonarqube workflow

--- index.ts
+++ index.ts
@@ -1,14 +1,11 @@
 import { APIMaker } from "./api";
 import { RecognizeController, RecognizeFromIdController } from "./controllers";
-import { config } from "dotenv";
-import { IComplexRecognition } from "./ocr/models";
-config()
 let port:number;
 
-if(process.env.REACT_APP_OCR_ENGINE_PORT === undefined){
+if(process.env.OCR_ENGINE_PORT === undefined){
     port = 3000;
 }else{
-    port = parseInt(process.env.REACT_APP_OCR_ENGINE_PORT,10)
+    port = parseInt(process.env.OCR_ENGINE_PORT,10)
 }
 
 const api = new APIMaker(port)
