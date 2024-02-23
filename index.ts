6ff2ce3ddcc11d634161057e9fc5b31e9e0b3ffc
MatthieuLvsr <72619668+MatthieuLvsr@users.noreply.github.com>
2024-02-23 10:45:57 +0100

    re-launch api

--- index.ts
+++ index.ts
@@ -1,8 +1,17 @@
 import { APIMaker } from "./api";
 import { RecognizeController, RecognizeFromIdController } from "./controllers";
 import { DefaultController } from "./controllers/default.controller";
+let port:number;
+
+// if(process.env.OCR_ENGINE_PORT === undefined){
+//     port = 80;
+// }else{
+//     port = parseInt(process.env.OCR_ENGINE_PORT,10)
+// }
 
 const api = new APIMaker()
 api.SetupControllers([RecognizeController.getInstance(),RecognizeFromIdController.getInstance(),DefaultController.getInstance()])
+api.LaunchAPI()
+
 
 module.exports = api.app
