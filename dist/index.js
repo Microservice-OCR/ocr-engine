1f360fe66e661593ada833f0646776efd8ea8ed0
MatthieuLvsr <72619668+MatthieuLvsr@users.noreply.github.com>
2024-02-24 11:33:30 +0100

    setup mode

--- dist/index.js
+++ dist/index.js
@@ -4,12 +4,22 @@ const api_1 = require("./api");
 const controllers_1 = require("./controllers");
 const default_controller_1 = require("./controllers/default.controller");
 let port;
-// if(process.env.OCR_ENGINE_PORT === undefined){
-//     port = 3000;
-// }else{
-//     port = parseInt(process.env.OCR_ENGINE_PORT,10)
-// }
-const api = new api_1.APIMaker();
-api.SetupControllers([controllers_1.RecognizeController.getInstance(), controllers_1.RecognizeFromIdController.getInstance(), default_controller_1.DefaultController.getInstance()]);
-api.LaunchAPI();
+let api = null;
+if (process.env.OCR_ENGINE_PORT === undefined) {
+    port = 3000;
+}
+else {
+    port = parseInt(process.env.OCR_ENGINE_PORT, 10);
+}
+if (process.env.MODE === undefined || process.env.MODE === 'DEV') {
+    console.log("OCR Engine is running on port " + port);
+    api = new api_1.APIMaker(port);
+}
+else if (process.env.MODE === 'PROD') {
+    api = new api_1.APIMaker();
+}
+if (api !== null) {
+    api.SetupControllers([controllers_1.RecognizeController.getInstance(), controllers_1.RecognizeFromIdController.getInstance(), default_controller_1.DefaultController.getInstance()]);
+    api.LaunchAPI();
+}
 // module.exports = api.app
--- index.ts
+++ index.ts
@@ -2,15 +2,24 @@ import { APIMaker } from "./api";
 import { RecognizeController, RecognizeFromIdController } from "./controllers";
 import { DefaultController } from "./controllers/default.controller";
 let port:number;
+let api:APIMaker|null = null
 
-// if(process.env.OCR_ENGINE_PORT === undefined){
-//     port = 3000;
-// }else{
-//     port = parseInt(process.env.OCR_ENGINE_PORT,10)
-// }
+if(process.env.OCR_ENGINE_PORT === undefined){
+    port = 3000;
+}else{
+    port = parseInt(process.env.OCR_ENGINE_PORT,10)
+}
 
-const api = new APIMaker()
-api.SetupControllers([RecognizeController.getInstance(),RecognizeFromIdController.getInstance(),DefaultController.getInstance()])
-api.LaunchAPI()
+if(process.env.MODE === undefined || process.env.MODE === 'DEV'){
+    console.log("OCR Engine is running on port "+port)
+    api = new APIMaker(port)
+}else if(process.env.MODE === 'PROD'){
+    api = new APIMaker()
+}
+
+if(api !== null){
+    api.SetupControllers([RecognizeController.getInstance(),RecognizeFromIdController.getInstance(),DefaultController.getInstance()])
+    api.LaunchAPI()
+}
 
 // module.exports = api.app
